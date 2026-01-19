dfd0c69acf10aa5bf3ff8cfec05b3a3e3daf5751
Istiak Kashem Jisan <istiakkashem35@gmail.com>
2026-01-20 03:20:16 +0600

    build api to get all subjects

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,11 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to the PERN Classroom Backend!" });
 });
 
+// Import and use subjects routes
+import subjectRoutes from "./routes/subjects";
+
+app.use("/api/subjects", subjectRoutes);
+
 // Start server
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
